bd10f895cb0be9cb322ca41bc2530983db46dc1d
Elizabeth Silvia Pumacota Quispe <elizabethsilvia.15@gmail.com>
2022-09-22 20:27:09 -0500

    Se agrega el prepared statatement

--- src/main/java/com/isil/sesion1/MainApp.java
+++ src/main/java/com/isil/sesion1/MainApp.java
@@ -39,7 +39,7 @@ public class MainApp {
         //5. RECORRER RESULTADO
 
 
-
+/*
         //CREANDO UN NUEVO STATEMENT
         Statement stmt2= con.createStatement();
         ResultSet resultSet = stmt2.executeQuery("select * from Users where name='Ronaldo'");
@@ -50,6 +50,68 @@ public class MainApp {
                         resultSet.getString("phone")+"\n"+
                         resultSet.getString("city"));
         }
+
+        //USANDO PREARE STATEMENT
+
+ */
+/*
+        PreparedStatement preparedStatement =
+                con.prepareStatement("select * from Users where name=?");
+
+        preparedStatement.setString(1,"Jugador3");
+
+        ResultSet resultSet2 = preparedStatement.executeQuery();
+
+        while (resultSet2.next()){
+            System.out.println(resultSet2.getString("name")+ "\n"+
+                    resultSet2.getString("phone")+"\n"+
+                    resultSet2.getString("city"));
+        }
+*/
+
+        /*
+
+        //USANDO PREARE STATEMENT con dos parametros de busqueda
+
+        PreparedStatement preparedStatement =
+                con.prepareStatement("select * from Users where name=? and city=?");
+
+        preparedStatement.setString(1,"Jugador3");
+        preparedStatement.setString(2,"Medellin");
+
+        ResultSet resultSet2 = preparedStatement.executeQuery();
+
+        while (resultSet2.next()){
+            System.out.println(resultSet2.getString("name")+ "\n"+
+                    resultSet2.getString("phone")+"\n"+
+                    resultSet2.getString("city"));
+        }
+*/
+        /*
+        //USANDO STATEMENT E INSERTANDO UN DATO (FILAS DE MANTENIMIENTO)
+
+        Statement stCreate = con.createStatement();
+        int filasAfectadas=
+                stCreate.executeUpdate("INSERT INTO Users VALUES (5,'Elizabeth','34667852','Tokio')");
+        System.out.println("Filas afectadas: "+ filasAfectadas);
+        PreparedStatement preparedStatement =
+                con.prepareStatement("select * from Users where name=?");
+
+        preparedStatement.setString(1,"Elizabeth");
+
+        ResultSet resultSet2 = preparedStatement.executeQuery();
+        //Recorre los resultados
+        while (resultSet2.next()){
+            System.out.println(resultSet2.getString("name") + "\n"+
+                    resultSet2.getString("phone") +"\n"+
+                    resultSet2.getString("city"));
+        }
+*/
+
+        //USANDO PREPARED STATEMENT - UPDATE (FILAS DE MANTENIMIENTO)
+
+
+
 
         //6.CERRAR CONEXION
 
